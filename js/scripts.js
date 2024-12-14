d9513fd12a168cf89e556c4736b679fd0ad09ebd
Ziedsabri <ziedsabri1920@gmail.com>
2024-12-14 12:22:02 +0100

    charger le projet

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+function togglePassword() {
+    
+    const passwordField = document.getElementById('password');
+    const eyeIcon = document.getElementById('eyeIcon');
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';  
+        eyeIcon.classList.remove('fa-eye');  
+        eyeIcon.classList.add('fa-eye-slash');  
+    } else {
+        passwordField.type = 'password';  
+        eyeIcon.classList.remove('fa-eye-slash');  
+        eyeIcon.classList.add('fa-eye');  
+    }
+}
+
+
